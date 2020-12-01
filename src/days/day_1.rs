aa25a4984f0c708d78b8ab41defa25a749d1ef89
David Richardson <david@tankski.co.uk>
2020-12-01 15:10:07 +0000

    Optimise day 1 answer

--- src/days/day_1.rs
+++ src/days/day_1.rs
@@ -1,6 +1,5 @@
 use std::time::SystemTime;
 use itertools::Itertools;
-use std::collections::HashSet;
 use std::fs;
 
 use crate::Path;
@@ -9,22 +8,30 @@ pub fn execute(input: &Path) {
     println!("Starting day 1");
 
     let start_time = SystemTime::now();
-    let data: Vec<i32> = fs::read_to_string(input)
+    let data_raw: Vec<i32> = fs::read_to_string(input)
         .expect("Could not read the input file")
-        .split("\n")
-        .filter(|s| s.len() > 0)
+        .lines()
         .map(|s| i32::from_str_radix(s, 10).expect("Failed to parse integer value"))
         .sorted()
         .collect();
+
+    let data: Vec<i32> = data_raw
+        .iter()
+        .cloned()
+        .filter(|x| x + data_raw.get(0).unwrap() <= 2020)
+        .collect();
+
     let setup_time = SystemTime::now();
 
-    let part_1_ans = part_1(&data);
+    let part_1_ans = get_combinations(&data, 2);
     let part_1_time = SystemTime::now();
 
-    let part_2_ans = part_2(&data);
+    let part_2_ans = get_combinations(&data, 3);
     let part_2_time = SystemTime::now();
 
-    println!("\nPart 1: {}, part 2: {}", part_1_ans, part_2_ans);
+    println!("Part 1 numbers: {:?}, answer: {:?}", part_1_ans, part_1_ans.iter().cloned().fold1(|x, y| x*y).unwrap());
+    println!("Part 2 numbers: {:?}, answer: {:?}", part_2_ans, part_2_ans.iter().cloned().fold1(|x, y| x*y).unwrap());
+
     println!("\nTime beakdowns:\n\nSetup: {:?}\nPart 1: {:?}\nPart 2: {:?}\nTotal: {:?}",
         setup_time.duration_since(start_time).unwrap(),
         part_1_time.duration_since(setup_time).unwrap(),
@@ -32,38 +39,10 @@ pub fn execute(input: &Path) {
         part_2_time.duration_since(start_time).unwrap());
 }
 
-fn part_1(data: &Vec<i32>) -> String {
-    let data_set: HashSet<i32> = data.iter().cloned().collect();
-    let all_pairs: Vec<(&i32, &i32)> = data_set
-        .iter()
-        .cartesian_product(data_set.iter())
-        .unique()
-        .collect();
-
-    let answer = all_pairs
-        .iter()
-        .find(|(l, r)| (*l + *r) == 2020)
-        .expect("Could not find a pair of values that sums to 2020");
-
-    format!("{}", answer.0 * answer.1)
-}
-
-fn part_2(data: &Vec<i32>) -> String {
-    let data: Vec<_> = vec![data.iter(), data.iter(), data.iter()];
-
-    let all_triples: Vec<(&i32, &i32, &i32)> = data
-        .iter()
+fn get_combinations(nums: &Vec<i32>, size: usize) -> Vec<i32> {
+    nums.iter()
         .cloned()
-        .multi_cartesian_product()
-        .unique()
-        .map(|v| (v[0], v[1], v[2]))
-        .collect();
-
-    let answer = all_triples
-        .iter()
-        .cloned()
-        .find(|(l, m, r)| (*l + *m + *r) == 2020)
-        .expect("Could not find a triple of values that sums to 2020");
-
-    format!("{}", answer.0 * answer.1 * answer.2)
+        .combinations(size)
+        .find(|v| v.iter().sum::<i32>() == 2020)
+        .unwrap()
 }
