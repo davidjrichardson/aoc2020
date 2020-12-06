30819f6c3f15b9ec2837088e70cdef3fe4285119
David Richardson <david@tankski.co.uk>
2020-12-06 10:15:57 +0000

    Optimise day 6

--- src/days/day_6.rs
+++ src/days/day_6.rs
@@ -1,12 +1,10 @@
+use std::fs;
 use std::path::Path;
-use std::{collections::HashSet, fs};
 
 use crate::days::Challenge;
 
-type DeclForm = Vec<HashSet<char>>;
-
 pub struct Day6<'a> {
-    pub(crate) data: Vec<DeclForm>,
+    pub(crate) data: Vec<Vec<u32>>,
     pub(crate) file_path: &'a Path,
     pub(crate) part_1_ans: Option<usize>,
     pub(crate) part_2_ans: Option<usize>,
@@ -23,34 +21,39 @@ impl<'a> Day6<'_> {
     }
 }
 
+fn mask_union(group: &Vec<u32>) -> u32 {
+    group.iter().fold(0, |acc, v| acc | v).count_ones()
+}
+
+fn mask_intersection(group: &Vec<u32>) -> u32 {
+    group.iter().fold(u32::MAX, |acc, v| acc & v).count_ones()
+}
+
 impl Challenge<'_> for Day6<'_> {
     fn setup(&mut self) {
         let raw_str = fs::read_to_string(self.file_path).unwrap();
 
         self.data = raw_str
             .split("\n\n")
-            .map(|c| c.split_whitespace().map(|x| x.chars().collect()).collect())
+            .map(|c| {
+                c.lines()
+                    .map(|x| x.as_bytes().iter().map(|b| 1 << (b - 97u8)).sum())
+                    .collect()
+            })
             .collect();
     }
 
     fn part_1(&mut self) {
-        self.part_1_ans = Some(self.data.iter().fold(0, |acc, v| {
-            acc + v
-                .iter()
-                .fold(HashSet::new(), |a, s| a.union(s).cloned().collect())
-                .len()
-        }));
+        self.part_1_ans = Some(self.data.iter().map(|g| mask_union(g) as usize).sum());
     }
 
     fn part_2(&mut self) {
-        self.part_2_ans = Some(self.data.iter().fold(0, |acc, v| {
-            acc + v
+        self.part_2_ans = Some(
+            self.data
                 .iter()
-                .fold(v.get(0).unwrap().clone(), |a, s| {
-                    a.intersection(s).cloned().collect()
-                })
-                .len()
-        }));
+                .map(|g| mask_intersection(g) as usize)
+                .sum(),
+        );
     }
 
     fn format_answers(&self) -> String {
